3702647aa6d91327b72e4cec4b3f15a1e093da8d
Philip Greene <pgreene@spartaglobal.com>
2019-08-28 22:39:42 +0100

    Declared methods for 'Simple Calculator'

--- CalculatorOOP/Program.cs
+++ CalculatorOOP/Program.cs
@@ -10,20 +10,8 @@ namespace CalculatorOOP
     {
         static void Main(string[] args)
         {
-            // User selects which calculator they want to use
-            switch (ChooseCalculator())
-            {
-                case 1:
-                    Console.WriteLine("You have chosen 'Simple Calculator'");
-                    SimpleCalculator simpleCalc = new SimpleCalculator();
-                    break;
-                case 2:
-                    Console.WriteLine("You have chosen 'BMI Calculator'");
-                    BMICalculator bmiCalc = new BMICalculator();
-                    break;
-                default:
-                    break;
-            }
+            // User runs the calculator they want to use
+            RunCalculator(ChooseCalculator());
         }
 
         private static int ChooseCalculator()
@@ -36,15 +24,93 @@ namespace CalculatorOOP
             int userSelection = int.Parse(Console.ReadLine());
             return userSelection;
         }
+
+        private static void RunCalculator(int userSelection)
+        {
+            switch (userSelection)
+            {
+                case 1:
+                    Console.WriteLine("You have chosen 'Simple Calculator'");
+                    SimpleCalculator simpleCalc = new SimpleCalculator();
+                    simpleCalc.GetInputValues(userSelection);
+                    
+                    
+                    break;
+                case 2:
+                    Console.WriteLine("You have chosen 'BMI Calculator'");
+                    BMICalculator bmiCalc = new BMICalculator();
+                    bmiCalc.GetInputValues(userSelection);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    class SimpleCalculator
+    class Calculator
     {
+        
+        public double firstNumber = 0;
+        public double secondNumber = 0;
 
+        public void GetInputValues(int userSelection)
+        {
+            switch (userSelection)
+            {
+                case 1:
+                    Console.Write("Please Enter your first number (double): ");
+                    firstNumber = double.Parse(Console.ReadLine());
+                    Console.Write("Please Enter your second number (double): ");
+                    secondNumber = double.Parse(Console.ReadLine());
+                    break;
+                case 2:
+                    Console.Write("Please enter your height in meters(m) (e.g. 1.8) (double): ");
+                    firstNumber = double.Parse(Console.ReadLine());
+                    Console.Write("Please enter your weight in kilograms(kg) (e.g. 75) (double): ");
+                    secondNumber = double.Parse(Console.ReadLine());
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+    class SimpleCalculator : Calculator
+    {
+        // Could make RunCalculator a virtual method in the base class to be overriden in the child class
+        public void RunCalculator()
+        {
+            SelectOperation();
+        }
+
+        public int SelectOperation()
+        {
+            return 0;
+        }
+
+        public double Add()
+        {
+            return 0;
+        }
+        public double Subtract()
+        {
+            return 0;
+        }
+        public double Multiply()
+        {
+            return 0;
+        }
+        public double Divide()
+        {
+            return 0;
+        }
     }
 
-    class BMICalculator
+    class BMICalculator : Calculator
     {
+        public void RunCalculator()
+        {
 
+        }
     }
 }
